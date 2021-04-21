aa55c44053140ceb7302cbc29df65b63ec346647
Alimov Tillo <alimovtillo@gmail.com>
2021-04-21 22:07:13 +0600

    added Passport model

--- app/app/admin.py
+++ app/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Passport
+
+admin.site.register(Passport)
--- app/app/models.py
+++ app/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Passport(models.Model):
+    firstname = models.CharField(max_length=32)
+    lastname = models.CharField(max_length=32)
+    middlename = models.CharField(max_length=32)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=32)
+    tin = models.IntegerField()
+    
+
+    def __str__(self):
+        return "%s - %s - %s" % (self.firstname,  self.lastname, self.middlename)
+    class Meta:
+        db_table = 'passport'
